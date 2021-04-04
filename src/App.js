3edccf172626bedec86690eb403ee8b4a6ee9684
Sophie Yu <yuxiaoping0405@gmail.com>
2021-04-04 18:34:55 -0400

    menu component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap'
+import Menu from './components/MenuComponent';
+import './App.css';
 
+// this is the App component
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div >
+        { /* render the Navbar component */ }
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+
+        {/* render the Menu component  */}
+        <Menu />
+
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//put index.css after bootstrap.min.css so it can override
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
+// render the App component in index.js
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
