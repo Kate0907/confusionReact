3e97b9cadf488379ebb2c81f292caa98ac65e14a
Sophie Yu <yuxiaoping0405@gmail.com>
2021-04-09 19:37:21 -0400

    add client comments

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,73 +1,46 @@
 import React, { Component } from 'react';
-import { Card,CardImg,CardImgOverlay,CardText,CardBody,CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import Dishdetail from './DishdetailComponent';
 
 // this is the Menu Component
 class Menu extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			selectedDish: null
+		};
+	}
+	//Change state of your component
+	onDishSelect(dish) {
+		this.setState({
+			selectedDish: dish
+		});
+	}
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            selesctedDish:null
-        }
-    }
-//Change state of your component
-    onDishSelect(dish) {
-        this.setState({ selesctedDish: dish });
-    }
-
-    renderDish(dish) {
-        if (dish != null) {
-            return (
-                <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name} </CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            )
-        }
-        else {
-            return (
-                <div className="col-12 col-md-5 m-1">
-                    <Card>Currently no dish selected!</Card>
-                </div>
-                
-            )
-        }
-    }
-
-    render() {
-// map: go through the dishes arrey, for every dish I will do the return=>
-        const menu = this.props.dishes.map((dish) => {
-            
-            return (
-                //each items should have a unique key 
-                <div key={dish.id} className="col-12 col-md-5 m-1">
-                    <Card onClick={() =>this.onDishSelect(dish) }>
-                        <CardImg width="100%" src={dish.image} alt={dish.name}/>
-                        <CardImgOverlay>
-                            <CardTitle>{dish.name} </CardTitle>  
-                        </CardImgOverlay>
-                    </Card>
-                </div>
-            )
-        });
-        
-        
-        return (
-            <div className="container">
-                <div className="row">               
-                    {menu}                    
-                </div>
-                <div className="row">
-                    {this.renderDish(this.state.selesctedDish)}
-                </div>
-            </div>
-            
-        );
-    }
-    
+	render() {
+		// map: go through the dishes arrey, for every dish I will do the return=>
+		const menu = this.props.dishes.map((dish) => {
+			return (
+				//each items should have a unique key
+				<div key={dish.id} className="col-12 col-md-5 m-1">
+					<Card onClick={() => this.onDishSelect(dish)}>
+						<CardImg width="100%" src={dish.image} alt={dish.name} />{' '}
+						<CardImgOverlay>
+							<CardTitle> {dish.name} </CardTitle>{' '}
+						</CardImgOverlay>{' '}
+					</Card>{' '}
+				</div>
+			);
+		});
+		console.log('Menu component:');
+		console.log(this.state.selectedDish);
+		return (
+			<div className="container">
+				<div className="row"> {menu} </div>
+				<Dishdetail dishFromMenu={this.state.selectedDish} />{' '}
+			</div>
+		);
+	}
 }
 
 export default Menu;
